99f7fa933ee03159e095db47d21b40152ab2dd0c
RidvanBekir <ridvan1994@abv.bg>
2014-05-07 00:20:49 +0300

    Wrong file

---
+++ 07.Arrays/12.AlphabetArray/AlphabetArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class AlphabetArray
+{
+    static void Main()
+    {
+        int[] letters = new int[53];
+        for (int i = 1; i < letters.Length / 2 + 1; i++)
+        {
+            letters[i] = ('a' - 1) + i;
+        }
+        for (int i = letters.Length / 2 + 1, j = 0; i < letters.Length; i++, j++)
+        {
+            letters[i] = 'A' + j;
+        }
+
+        Console.WriteLine("Enter a word");
+        string word = Console.ReadLine();
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int j = 0; j < letters.Length; j++)
+            {
+                if (word[i] == letters[j])
+                {
+                    Console.WriteLine("{0} has index {1} ", word[i], letters[j]);
+                    break;
+                }
+            }
+        }
+    }
+}
